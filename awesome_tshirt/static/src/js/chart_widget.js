4a39f5fcb39154b449d518660345a53fbce7ad72
sirquist <squist@quantumgroupgh.com>
2018-10-02 10:33:56 +0000

    Task 2.3

--- awesome_tshirt/static/src/js/chart_widget.js
+++ awesome_tshirt/static/src/js/chart_widget.js
@@ -58,7 +58,7 @@ var ChartWidget = Widget.extend({
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
                         'rgba(255, 206, 86, 0.2)',
-                        'rgba(0,0,255,0.3)',
+                        'rgba(75, 192, 192, 0.2)',
                     ],
                     borderColor: [
                         'rgba(255,99,132,1)',
